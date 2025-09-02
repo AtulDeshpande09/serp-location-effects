b9b43640df4c36e353021f1bc578c5520e98d261
AtulDeshpande09 <deshpandeatul05@gmail.com>
2025-09-03 00:03:03 +0530

    test run

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-#from serpapi import GoogleSearch
+from serpapi import GoogleSearch
+import json
 
 # List of locations
 locations = open("locations.txt",'r')
@@ -9,8 +10,36 @@ queries = open("queries.txt",'r')
 query_list = [query.strip('\n') for query in queries]
 
 
+def search_web(query,location,num):
+
+    params = {
+            'q' : query,
+            'location': location,
+            'google_domain': 'google.com',
+            'num' : num,
+            'output' : 'json',
+            'api_key' : "API_KEY"
+            }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    return results
+
+
 
 if __name__ == "__main__":
 
-    print(loc_list)
-    print(query_list)
+    file_path = "test.json"
+    
+    l = "India"
+    q = "Coffee"
+    num = 10
+    results = search_web(q,l,num)
+    print("Results : ",results)
+    print()
+    print("Type ",type(results))
+    
+    with open(file_path,'w') as file:
+        json.dump(results,file,indent=4)
+    print("File Saved Succefully!!!")
+    
