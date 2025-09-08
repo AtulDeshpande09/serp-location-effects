f418876d8e7d2fb733426f5c26f78b274d9404de
Atul Deshpande <deshpandeatul05@gmail.com>
2025-09-08 18:54:59 +0530

    Revert "fixed utils"

--- scripts/merge_data.py
+++ scripts/merge_data.py
@@ -1,13 +1,5 @@
 import json
 
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-
-from project_utils.preprocessing import get_organic_search_results, get_titles , get_links , get_snippets
-
-from project_utils.metadata import get_search_metadata
-
 # List of locations
 locations = open("locations.txt",'r')
 loc_list = [location.strip('\n') for location in locations]
@@ -15,6 +7,48 @@ loc_list = [location.strip('\n') for location in locations]
 # List of Queries
 queries = open("queries.txt",'r')
 query_list = [query.strip('\n') for query in queries]
+
+
+def curate_metadata(data):
+    
+    metadata_dic = {}
+    metadata = data['search_metadata']
+    
+    metadata_dic['id'] = metadata["id"]
+    metadata_dic["created_at"] = metadata["created_at"]
+    metadata_dic["processed_at"] = metadata["processed_at"]
+    metadata_dic["time_taken"] = metadata["total_time_taken"]
+
+    metadata = data["search_parameters"]
+    
+    metadata_dic["location"] = metadata["location_used"]
+    metadata_dic["query"] = metadata["q"]
+    metadata_dic["search_engine"] = metadata['engine']
+    
+    return metadata_dic
+
+
+def get_search_metadata(path_list, csv:bool=False):
+    
+    search_metadata_dic = {}
+    for path in path_list:
+
+        with open(path,'r') as f:
+            data = json.load(f)
+
+        data = curate_metadata(data)        
+        search_metadata_dic[path] = data
+
+    if csv:
+        import pandas as pd
+        df = pd.DataFrame(search_metadata_dic)
+        df = df.transpose()
+
+        df.to_csv("../Data/metadata.csv",index=False)
+    
+        print("Metadata csv created!!!")
+
+    return search_metadata_dic
 
 
 def create_path(location,query):
@@ -31,18 +65,40 @@ def create_path(location,query):
 
 
 
+def all_json_file(path_list):
+
+    combined = {query:{} for query in query_list}
+ 
+    for query in query_list:
+
+        combined[query] = {}
+
+        for location in loc_list:
+            path = location+"/"+query+".json"
+            path = f'../Data/{path}'
+            if path in path_list:
+                
+                with open(path,'r') as f:
+                    data = json.load(f)
+                data = data['organic_results']
+
+                data = {location : data}
+
+                combined[query].update(data)
+        
+    return combined
+    
+
+
 if __name__ == "__main__":
 
     paths = create_path(loc_list,query_list)
 
-    combined = get_organic_search_results(paths,query_list,loc_list)
+    combined = all_json_file(paths)
 
-    print(get_snippets(combined))
-
-"""
 
     with open('../Data/merged.json','w') as file:
         json.dump( combined , file , indent=4)    
     print("File Stored successsfully!!!")
 
-    metadata_dic = get_search_metadata(a,csv=True) """
+    metadata_dic = get_search_metadata(a,csv=True) 
